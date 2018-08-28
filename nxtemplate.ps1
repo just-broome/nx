8878f04ad192e6ec7f07b6190f91a090d07e7be2
Broome <dboftlp@gmail.com>
2018-08-28 13:00:19 -0400

    nxtemplate init

---
+++ nxtemplate.ps1
@@ -0,0 +1,92 @@
+﻿#region File Header
+<# FileName: .ps1
+===============================================
+ 
+.SYNOPSIS
+        
+.DESCRIPTION
+        
+.PARAMETER
+
+.PARAMETER
+
+.PARAMETER
+
+.EXAMPLE
+        
+.LINK
+
+.NOTES
+    Author: David Broome (broome@nutanix.com)
+    Created: [DATE_MDY]
+    Revision: 
+    Company: Nutanix
+    Email: broome@nutanix.com
+    Web: http://www.nutanix.com
+    Reqrmnts: 
+    Keywords: 
+
+===============================================
+ REVISION HISTORY
+===============================================
+ Date: [DATE_MDY]
+ Time: [TIME]
+ Issue: 
+ Solution: 
+
+==============================================#>
+#endregion
+
+
+
+#region Parameters and initial setup
+<#=============================================
+PARAMETERS
+=============================================#>
+
+
+
+#endregion
+
+
+
+#region Functions
+<#=============================================
+FUNCTION LISTINGS
+===============================================
+
+.SYNOPSIS
+        
+.DESCRIPTION
+        
+.PARAMETER
+        
+.EXAMPLE
+        
+.LINK
+
+.NOTES
+    Author: David Broome (broome@nutanix.com)
+    Created: [DATE_MDY]
+    Revision: [DATE_MDY]
+
+=============================================#>
+#endregion
+
+
+
+#region Script
+<#=============================================
+SCRIPT BODY
+=============================================#>
+
+
+
+
+
+
+
+<#=============================================
+END OF SCRIPT: [FILE_NAME]
+=============================================#>
+#endregion
